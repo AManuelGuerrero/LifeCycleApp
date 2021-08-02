6d9cbfd8b33b6e0143f8d04fb860aca8815f993c
Antonio Manuel Guerrero Trujillo <antonio.guerrero@atmira.com>
2021-08-02 09:04:10 +0200

    LifeCycleApp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PaginalComponent } from './pges/paginal/paginal.component';
+import { MuestraNombreComponent } from './components/muestra-nombre/muestra-nombre.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaginalComponent,
+    MuestraNombreComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
